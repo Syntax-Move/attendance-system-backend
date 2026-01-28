57d0610c8bc7f30e87d0c393559761c65c5d1b88
usama <safiullahrafeeq7777@gmail.com>
2026-01-28 14:39:29 +0500

    Enhance deployment workflow and improve logging
    Updated the GitHub Actions deploy workflow for clearer steps, environment-specific PM2 process names, and improved NVM loading. Also enhanced application startup logs to include the current NODE_ENV for better clarity during deployment and debugging.

--- src/main.ts
+++ src/main.ts
@@ -56,7 +56,7 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}`);
-  console.log(`Swagger documentation available at: http://localhost:${port}/docs`);
+  console.log(`Application is running on: http://localhost:${port} for ${process.env.NODE_ENV} environment`);
+  console.log(`Swagger documentation available at: http://localhost:${port}/docs for ${process.env.NODE_ENV} environment`);
 }
 bootstrap();
