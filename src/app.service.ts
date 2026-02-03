1fe4dabfe4c282d8733ff2d0d6591719a3be15d8
usama <safiullahrafeeq7777@gmail.com>
2026-02-03 17:36:49 +0500

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `Hello World! from ${process.env.NODE_ENV} environment  update 01`;
+    return `Hello World! from ${process.env.NODE_ENV} environment  update 02`;
   }
 }
